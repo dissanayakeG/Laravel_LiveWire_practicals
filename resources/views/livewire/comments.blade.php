91ef76a91837f584048f5c221d6e8c9a5dbef83f
Madusanka Dissanayake <mdissanayake994@gmail.com>
2023-02-14 13:16:05 +0530

    add tailwind and remove boostrap

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,14 +1,13 @@
-<div class="container">
-    <form class="container form-inline" style="margin-top: 20px" wire:submit.prevent="addComment">
-        <div class="form-group">
+<div class="container"> 
+    <form class="flex items-center" >      
+        <div class="flex items-center">
             {{--        <input type="text" class="form-control" id="comment" wire:model.debounce.1000ms="newComment">--}}
-            <input type="text" class="form-control input-lg" id="comment" wire:model.debounce.lazy="newComment">
-        </div>
-        <button wire:click="addComment" class="btn btn-primary">Submit</button>
-    </form>
-
+            <input class="h-10 px-3 rounded-full border-gray-300 text-sm focus:outline-none" type="text"  id="comment" placeholder="Enter some comment" wire:model.debounce.lazy="newComment">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" wire:click.prevent="addComment">Submit</button>
+          </div>       
+      </form>     
     <div class="container">
-        @error('newComment') <span class="text-danger">{{ $message }}</span> @enderror
+        @error('newComment') <span class="text-red-600">{{ $message }}</span> @enderror
     </div>
 
     <div class="container" style="margin-top: 20px">
@@ -20,7 +19,7 @@
                     <h6>{{$comment->created_at->diffForHumans()}}</h6>
                     <h5>{{$comment->body}}</h5>
                 </div>
-                <div class="text-danger delete-btn" wire:click="deleteComment({{$comment->id}})">X</div>
+                <div class="text-red-600 delete-btn" wire:click="deleteComment({{$comment->id}})">X</div>
             </div>
 
         @endforeach
