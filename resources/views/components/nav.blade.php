3635de09c6532cd7f08e755d899be17a6a018549
Madusanka Dissanayake <mdissanayake994@gmail.com>
2023-10-06 12:32:44 +0530

    add auth middleware

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,56 +1,60 @@
 <div class="nav-list">
     <ul>
-        <li class="item">
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <a :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+        @auth
+            <li class="item">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                        <ion-icon name="chevron-forward-outline"></ion-icon>
+                        <span class="text">{{ 'Log Out' }}</span>
+                    </a>
+                </form>
+            </li>
+
+            <li class="item">
+                <a href="/dashboard" wire:navigate>
                     <ion-icon name="chevron-forward-outline"></ion-icon>
-                    <span class="text">{{ 'Log Out' }}</span>
+                    <span class="text">{{ 'DASHBOARD' }}</span>
                 </a>
-            </form>
-        </li>
-        <li class="item">
-            <a href="/register" wire:navigate>
-                <ion-icon name="chevron-forward-outline"></ion-icon>
-                <span class="text">{{ 'REGISTER' }}</span>
-            </a>
-        </li>
-        <li class="item">
-            <a href="/login" wire:navigate>
-                <ion-icon name="chevron-forward-outline"></ion-icon>
-                <span class="text">{{ 'LOGIN' }}</span>
-            </a>
-        </li>
-        <li class="item">
-            <a href="/dashboard" wire:navigate>
-                <ion-icon name="chevron-forward-outline"></ion-icon>
-                <span class="text">{{ 'DASHBOARD' }}</span>
-            </a>
-        </li>
-        <li class="item">
-            <a href="/livewire" wire:navigate>
-                <span class="text">{{ 'LIVEWIRE' }}</span>
-            </a>
-        </li>
-        <li class="item">
-            <a href="/form" wire:navigate>
-                <span class="text">{{ 'FORM SUBMISSION' }}</span>
-            </a>
-        </li>
-        <li class="item">
-            <a href="/events" wire:navigate>
-                <span class="text">{{ 'EVENTS' }}</span>
-            </a>
-        </li>
-        <li class="item">
-            <a href="/blade-tests" wire:navigate>
-                <span class="text">{{ 'BLADES' }}</span>
-            </a>
-        </li>
-        <li class="item">
-            <a href="/facades" wire:navigate>
-                <span class="text">{{ 'FACADES' }}</span>
-            </a>
-        </li>
+            </li>
+            <li class="item">
+                <a href="/livewire" wire:navigate>
+                    <span class="text">{{ 'LIVEWIRE' }}</span>
+                </a>
+            </li>
+            <li class="item">
+                <a href="/form" wire:navigate>
+                    <span class="text">{{ 'FORM SUBMISSION' }}</span>
+                </a>
+            </li>
+            <li class="item">
+                <a href="/events" wire:navigate>
+                    <span class="text">{{ 'EVENTS' }}</span>
+                </a>
+            </li>
+            <li class="item">
+                <a href="/blade-tests" wire:navigate>
+                    <span class="text">{{ 'BLADES' }}</span>
+                </a>
+            </li>
+            <li class="item">
+                <a href="/facades" wire:navigate>
+                    <span class="text">{{ 'FACADES' }}</span>
+                </a>
+            </li>
+        @else
+            <li class="item">
+                <a href="/register" wire:navigate>
+                    <ion-icon name="chevron-forward-outline"></ion-icon>
+                    <span class="text">{{ 'REGISTER' }}</span>
+                </a>
+            </li>
+            <li class="item">
+                <a href="/login" wire:navigate>
+                    <ion-icon name="chevron-forward-outline"></ion-icon>
+                    <span class="text">{{ 'LOGIN' }}</span>
+                </a>
+            </li>
+        @endauth
     </ul>
 </div>
