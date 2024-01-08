38b4121f760242605c87809bf127f3751ca24762
Madusanka Dissanayake <mdissanayake994@gmail.com>
2024-01-08 17:16:24 +0530

    alpine

--- resources/views/livewire/alpine.blade.php
+++ resources/views/livewire/alpine.blade.php
@@ -2,12 +2,11 @@
 
     <x-alpine.todo-list method="increment" />
 
-    <div class="flex mt-6 justify-center">
+    <div class="mt-6 flex justify-center">
 
         <x-alpine.multi-select :selectedOptions="$this->selectedOptions"
             :allOptions="$this->allOptions" />
 
         <button wire:click="submit">call</button>
-
     </div>
 </div>
