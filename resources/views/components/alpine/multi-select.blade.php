d1aeac5f42866e82e523eacadc6a7a9ac21147b1
Madusanka Dissanayake <mdissanayake994@gmail.com>
2024-01-08 16:47:19 +0530

    alpine

--- resources/views/components/alpine/multi-select.blade.php
+++ resources/views/components/alpine/multi-select.blade.php
@@ -8,28 +8,37 @@
             allOptions: @entangle('allOptions'),
             selectedOptionNames: ''
         }"
-        x-init="$watch('selectedOptions', value =>
+        x-init="selectedOptionNames = allOptions.filter(option => selectedOptions.includes(option.id.toString()) || selectedOptions.includes(option.id)).map(option => option.name).join(', ')
+        $watch('selectedOptions', value =>
             selectedOptionNames = allOptions.filter(option => selectedOptions.includes(option.id.toString()) || selectedOptions.includes(option.id)).map(option => option.name).join(', ')
         )">
 
 
         <input type="text"
             x-model="selectedOptionNames"
-            @click="open = true">
+            @click="open = true"
+            class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none">
 
         <div x-show="open"
+            class="bg-white-500 mt-2 h-40 overflow-y-scroll rounded-md border p-2"
             @click.outside="open = false">
+
             <template x-for="option in allOptions"
                 :key="option.id"
                 x-show="open"
                 @click.outside="open = false">
+
                 <div class="flex p-2">
+
                     <input type="checkbox"
                         class="mr-2"
                         :value="option.id"
                         x-model="selectedOptions">
+
                     <li x-text="option.name"
-                        class="list-none"></li>
+                        class="list-none">
+                    </li>
+
                 </div>
             </template>
         </div>
